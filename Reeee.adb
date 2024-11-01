f906860af40ac672d81821f212c15c0a4a620a9d
Kiril <kirilstoyanov96@gmail.com>
2024-11-01 09:37:38 +0200

    Update Reeee.adb

--- Reeee.adb
+++ Reeee.adb
@@ -1,20 +1,20 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Delay; use Ada.Delay;
+with Ada.Calendar.Delays; use Ada.Calendar.Delays;
 
 procedure Main is
 
    procedure Print_Repeat is
    begin
       Put_Line("r"); 
-      Flush;          
+      Flush;
 
       loop
          Put_Line("e"); 
-         Flush;          
-         Delay(0.1);     
+         Flush;
+         Delay_For(Duration(0.1));
       end loop;
    end Print_Repeat;
 
 begin
-   Print_Repeat; 
+   Print_Repeat;
 end Main;
